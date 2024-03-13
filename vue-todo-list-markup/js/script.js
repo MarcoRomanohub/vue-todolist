9febceaef28df2eaf510d6a3476e1916219a6776
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-03-13 15:25:09 +0100

    [add] default array

---
+++ vue-todo-list-markup/js/script.js
@@ -0,0 +1,48 @@
+/*
+DEFAULT
+1. Creare un array di oggetti (che rappresentano ogni todo), con proprietà text diversa per ognuno e done true o false random
+
+MILESTONE 1
+2. Con un ciclo v-for (elemento, indice?) stampiamo i vari todo.text
+3. Condizione se done === true aggiungiamo una classe overline al todo.text (col ternario? v-if?)
+
+MILESTONE 2
+4. @click alla x richiamo funzione che fa uno splice del todo dall'array (mi sserve indice punto2?)
+
+MILESTONE 3
+5. Input con v-model al todo.text, al click pulsante viene creato un nuovo todo con text = al contenuto input e viene pushato nell'array di oggetti(.unshift) con done: false. Aggiungere il keyup.enter
+*/
+
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+
+    return{
+      todoList:[
+        {
+          text:'Fare la spesa',
+          done: false
+        },
+        {
+          text:'Prendere le medicine',
+          done: true
+        },
+        {
+          text:'Fare i compiti',
+          done: true
+        },
+        {
+          text:'Andare a correre',
+          done: false
+        },
+        {
+          text:'Pulire casa',
+          done: false
+        },
+      ]
+    }
+
+  }
+}).mount('#app')
